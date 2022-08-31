afb2e39519fe9e183fca25e19b48c94e1137a6c9
moheebarif2 <103864578+moheebarif2@users.noreply.github.com>
2022-08-31 18:38:51 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,27 +36,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
+global.vcardowner = ['923221975038'] //ur owner number
+global.ownername = "Moheeb Arif Khokhar" //ur owner name
+global.ytname = "YT: All Kind Videos" //ur yt chanel name
 global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
-global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
+global.location = "Pakistan Azad Kashmir " //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '9471421243' //ur tag number
-global.botname = 'CYBER-X TEST EDITION' //ur bot name
+global.owner = ['923221975038']
+global.ownertag = '923258672449' //ur tag number
+global.botname = 'MOHEEB ARIF' //ur bot name
 global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
 global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
 global.linkz = 'https://youtu.be/uzMbW5v9QLE'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🚀" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.aliveimg ="https://telegra.ph/file/71c859102f3625d06204b.jpg"
+global.packname = "MOHEEB ARIF Bot" //ur sticker watermark packname
+global.author = "MOHEEB ARIF KHOKHAR" //ur sticker watermark author
+global.aliveimg = "https://telegra.ph/file/59387a6814e12bd492866.jpg
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
-global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
+global.welcomeimg= "https://telegra.ph/file/eff6ac12274b48bb3e9cf.jpg"
 global.gbyeimg = "https://telegra.ph/file/844812c77704231c08a90.jpg"
 
 //BOT MEDIAS
